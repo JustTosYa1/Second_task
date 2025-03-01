d77cc2e222064c9359158e7f3554a9c99c056bd5
JustTosYa1 <d9263466376@gmail.com>
2025-03-01 15:25:57 +0300

    Решена третья задача.

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
 import sys
 import random
-from PyQt6 import QtWidgets, uic
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtCore import QRect
 
 
-class MainWindow(QMainWindow):
+class CircleDrawer(QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__()
-        uic.loadUi("UI.ui", self)
-        self.button = self.findChild(QPushButton, "generateButton")
+        super().__init__()
+        self.setMinimumSize(500, 300)
+        self.setMaximumSize(500, 300)
+        self.setWindowTitle("Рисователь кружков")
+
+        self.button = QPushButton("Создать окружности", self)
         self.button.clicked.connect(self.add_circle)
+        self.button.setGeometry(150, 120, 200, 40)
         self.circles = []
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        for (x, y, diameter) in self.circles:
-            painter.setBrush(QColor(255, 255, 0))
+        for (x, y, diameter, color) in self.circles:
+            painter.setBrush(color)
             painter.drawEllipse(QRect(x, y, diameter, diameter))
 
     def add_circle(self):
         diameter = random.randint(20, 100)
         x = random.randint(0, self.width() - diameter)
         y = random.randint(0, self.height() - diameter)
-        self.circles.append((x, y, diameter))
+        color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.circles.append((x, y, diameter, color))
         self.update()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = MainWindow()
+    window = CircleDrawer()
     window.show()
     sys.exit(app.exec())
